7265b2ca77e26e9230c10f8d73810b674ecee9eb
Hugh Kennedy <hughskennedy@gmail.com>
2015-05-02 15:22:27 +1000

    General improvements
    * Add message panel for errors
    * Add file opener

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ![](http://img.shields.io/badge/stability-stable-green.svg?style=flat)
 
 Atom plugin for selecting one or more require statements and
-opening up their documentation on nodejs.org/GitHub/npm.
+opening up their documentation on nodejs.org/GitHub/npm or
+opening their target files.
 
 ![atom-require-opener](http://i.imgur.com/OcKso1Z.gif)
 
@@ -12,9 +13,11 @@ Open the Command Palette, and run one of the following commands:
 
 * Require Opener: Open Selection on Npm
 * Require Opener: Open Selection on Github
+* Require Opener: Open Selection in Atom
 
 Node.js core modules will be opened on nodejs.org's API
-documentation in both cases.
+documentation for the two former cases, and ignored in
+the latter case.
 
 ## Keybindings
 
@@ -25,6 +28,7 @@ keymap file and include something along the following lines:
 '.workspace':
   'cmd-ctrl-shift-alt-g': 'require-opener:open-selection-on-npm'
   'cmd-ctrl-shift-alt-n': 'require-opener:open-selection-on-github'
+  'cmd-ctrl-shift-alt-o': 'require-opener:open-selection-in-atom'
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,31 +1,78 @@
+var messages
+
 exports.activate = activate
 
 function activate() {
-  atom.workspaceView.command('require-opener:open-selections-on-npm', open(true))
-  atom.workspaceView.command('require-opener:open-selections-on-github', open(false))
+  const Messages = require('atom-message-panel').MessagePanelView
+
+  messages = messages || new Messages({
+    title: 'Require Opener'
+  })
+
+  atom.workspaceView.command('require-opener:open-selections-on-npm', open('npm'))
+  atom.workspaceView.command('require-opener:open-selections-on-github', open('github'))
+  atom.workspaceView.command('require-opener:open-selections-in-atom', open('file'))
 }
 
-function open(npm) {
+function open(mode) {
   return function() {
-    const selected = require('atom-selected-requires')().map(base)
+    const editor   = atom.workspace.getActiveEditor()
+    const filename = editor.getPath()
+
+    messages.clear()
+
+    const Selected = require('atom-selected-requires')
     const relative = require('relative-require-regex')
     const core     = require('resolve/lib/core.json')
+    const resolve  = require('resolve')
     const opener   = require('opener')
+    const path     = require('path')
     const url      = require('url')
 
-    selected.filter(function(name) {
+    try {
+      var selected = Selected(editor)
+    } catch(e) {
+      return error(e)
+    }
+
+    const cores = selected.filter(function(name) {
       return core.indexOf(name) !== -1
-    }).forEach(function(name) {
+    })
+
+    const local = selected.filter(function(name) {
+      return relative().test(name)
+    })
+
+    const remote = selected.filter(function(name) {
+      return cores.indexOf(name) === -1
+          && local.indexOf(name) === -1
+    })
+
+    //
+    // Opening files in Atom:
+    //
+    if (mode === 'file') {
+      return local.concat(remote).forEach(function(name) {
+        var dirname = path.dirname(filename)
+
+        resolve(name, { basedir: dirname }, function(err, result) {
+          if (err) return error(err)
+          atom.workspace.open(result)
+        })
+      })
+    }
+
+    //
+    // Opening files on GitHub/npm/nodejs.org:
+    //
+    cores.map(base).forEach(function(name) {
       opener(
         url.resolve('https://nodejs.org/api/', name + '.html')
       )
     })
 
-    selected.filter(function(name) {
-      return core.indexOf(name) === -1
-         && !relative().test(name)
-    }).forEach(function(name) {
-      opener(npm
+    remote.map(base).forEach(function(name) {
+      opener(mode === 'npm'
         ? url.resolve('https://npmjs.com/package/', name)
         : url.resolve('http://ghub.io/', name)
       )
@@ -35,4 +82,24 @@ function open(npm) {
 
 function base(dir) {
   return dir.split('/')[0]
+}
+
+function error(err) {
+  const Message = require('atom-message-panel').PlainMessageView
+  const Lined   = require('atom-message-panel').LineMessageView
+
+  messages.attach()
+
+  if (!err.loc) {
+    messages.add(new Message({
+      message: err.message,
+      className: 'require-opener-error'
+    }))
+  } else {
+    messages.add(new Lined({
+      message: err.message,
+      className: 'require-opener-error',
+      line: err.loc.line
+    }))
+  }
 }
